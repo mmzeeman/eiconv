c1266072175784f88b61839c2cc551a38dcf2fea
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2013-02-21 10:55:25 +0100

    Added interface to handle incomplete conversions needed for chunking

--- c_src/eiconv_nif.c
+++ c_src/eiconv_nif.c
@@ -14,6 +14,8 @@
 static ErlNifResourceType *eiconv_cd_type = NULL;
 
 static ERL_NIF_TERM _atom_ok;
+static ERL_NIF_TERM _atom_done;
+static ERL_NIF_TERM _atom_more;
 static ERL_NIF_TERM _atom_error;
 static ERL_NIF_TERM _atom_enomem;
 static ERL_NIF_TERM _atom_eilseq;
@@ -33,7 +35,8 @@ static void descruct_eiconv_cd(ErlNifEnv *env, void *cd)
         iconv_close(((eiconv_cd *) cd)->cd);
 }
 
-static ERL_NIF_TERM eiconv_make_error(ErlNifEnv* env, int error_number) {
+static ERL_NIF_TERM 
+eiconv_make_error(ErlNifEnv* env, int error_number) {
     ERL_NIF_TERM error;
 
     if(error_number == EILSEQ) {
@@ -47,7 +50,8 @@ static ERL_NIF_TERM eiconv_make_error(ErlNifEnv* env, int error_number) {
     return enif_make_tuple2(env, _atom_error, error);
 }
  
-static ERL_NIF_TERM eiconv_open_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) 
+static ERL_NIF_TERM 
+eiconv_open_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) 
 {
     char tocode[CONV_DESC_LEN], fromcode[CONV_DESC_LEN];
     ErlNifBinary tocode_bin, fromcode_bin;
@@ -87,7 +91,8 @@ static ERL_NIF_TERM eiconv_open_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM
     return enif_make_tuple2(env, _atom_ok, conv_d);
 }
 
-static ERL_NIF_TERM eiconv_conv_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) 
+static ERL_NIF_TERM 
+eiconv_conv_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) 
 {
     eiconv_cd *cd;
     ErlNifBinary inbuf, outbuf;
@@ -142,11 +147,109 @@ static ERL_NIF_TERM eiconv_conv_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM
     return enif_make_tuple2(env, _atom_ok, enif_make_binary(env, &outbuf));
 }
 
+static ERL_NIF_TERM 
+eiconv_chunk_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) 
+{
+    eiconv_cd *cd;
+    ErlNifBinary inbuf, outbuf;
+    unsigned char *in, *out;
+    size_t inbytesleft, outbytesleft, outbufsize, rc;
+
+    if (!enif_get_resource(env, argv[0], eiconv_cd_type, (void **) &cd))
+        return enif_make_badarg(env);
+
+    if (((eiconv_cd *) cd)->cd == (iconv_t)(-1))
+        return enif_make_badarg(env);
+  
+    if (!enif_inspect_iolist_as_binary(env, argv[1], &inbuf))
+        return enif_make_badarg(env);
+
+    in = inbuf.data;
+    inbytesleft = inbuf.size;
+
+    outbufsize = inbytesleft + (inbytesleft/2); 
+    outbytesleft = outbufsize;
+
+    if(!enif_alloc_binary(outbufsize, &outbuf)) 
+        return enif_make_tuple2(env, _atom_error, _atom_enomem);
+
+    out = outbuf.data;
+    
+    do {
+        rc = iconv(cd->cd, (char **) &in, &inbytesleft, (char **) &out, &outbytesleft);
+        if(rc == 0) 
+            break;
+
+        if (errno == E2BIG) {
+            /* The result doesn't fit in outbuf, realloc and try again */
+            outbytesleft += outbufsize;
+            outbufsize *= 2;
+
+            if (!enif_realloc_binary(&outbuf, outbufsize)) {
+               enif_release_binary(&outbuf);
+               return enif_make_tuple2(env, _atom_error, _atom_enomem);
+            }
+
+            out = outbuf.data + (outbufsize - outbytesleft);
+        } else if (errno == EINVAL) {
+            /* we got an incomplete character sequence */
+            if(outbytesleft > 0)
+                enif_realloc_binary(&outbuf, outbufsize - outbytesleft);
+            return enif_make_tuple2(env, _atom_more, enif_make_binary(env, &outbuf));
+        } else {
+            enif_release_binary(&outbuf);
+            return eiconv_make_error(env, errno);
+        }
+    } while (rc != 0);
+
+    if(outbytesleft > 0)
+        enif_realloc_binary(&outbuf, outbufsize - outbytesleft);
+
+    return enif_make_tuple2(env, _atom_done, enif_make_binary(env, &outbuf));
+}
+
+/* 
+ * Set the cd structure to its initial state.
+ */
+static ERL_NIF_TERM 
+eiconv_reset_nif(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) 
+{
+    eiconv_cd *cd;
+    ErlNifBinary outbuf;
+    unsigned char *out;
+    size_t rc, outbytesleft=1024, outbufsize=1024;
+ 
+    if (!enif_get_resource(env, argv[0], eiconv_cd_type, (void **) &cd))
+        return enif_make_badarg(env);
+
+    if (((eiconv_cd *) cd)->cd == (iconv_t)(-1))
+        return enif_make_badarg(env);
+
+    if(!enif_alloc_binary(outbufsize, &outbuf)) 
+        return enif_make_tuple2(env, _atom_error, _atom_enomem);
+
+    /* Reset the cd structure */
+    out = outbuf.data;
+    rc = iconv(cd->cd, NULL, NULL, (char **) &out, &outbytesleft);
+    if(rc != 0) {
+        enif_release_binary(&outbuf);
+        return eiconv_make_error(env, errno);
+    }
+
+    /* Return leftover bytes */
+    if(outbytesleft > 0)
+        enif_realloc_binary(&outbuf, outbufsize - outbytesleft);
+
+    return enif_make_tuple2(env, _atom_ok, enif_make_binary(env, &outbuf));
+}
+
+
 /* 
  * loading
  */
 
-static int on_load(ErlNifEnv* env, void** priv, ERL_NIF_TERM info)
+static int 
+on_load(ErlNifEnv* env, void** priv, ERL_NIF_TERM info)
 {
     ErlNifResourceType *rt = enif_open_resource_type(
         env, "eiconv", "eiconv_cd_type", 
@@ -159,6 +262,8 @@ static int on_load(ErlNifEnv* env, void** priv, ERL_NIF_TERM info)
     /* Create some atoms 
     */
     _atom_ok = enif_make_atom(env, "ok");
+    _atom_more = enif_make_atom(env, "more");
+    _atom_done = enif_make_atom(env, "done");
     _atom_error = enif_make_atom(env, "error");
     _atom_enomem = enif_make_atom(env, "enomem");
     _atom_eilseq = enif_make_atom(env, "eilseq");
@@ -171,6 +276,8 @@ static int on_load(ErlNifEnv* env, void** priv, ERL_NIF_TERM info)
 static ErlNifFunc nif_funcs[] = {
     {"open", 2, eiconv_open_nif},
     {"conv", 2, eiconv_conv_nif},
+    {"chunk", 2, eiconv_chunk_nif},
+    {"reset", 1, eiconv_reset_nif}
 };
 
 ERL_NIF_INIT(eiconv, nif_funcs, on_load, NULL, NULL, NULL);
--- src/eiconv.erl
+++ src/eiconv.erl
@@ -5,7 +5,7 @@
 -module(eiconv).
 -author("Maas-Maarten Zeeman <mmzeeman@xs4all.nl>").
 
--export([open/2, conv/2, close/1]).
+-export([open/2, conv/2, chunk/2, reset/1, close/1]).
 
 % easy api one shot convert api
 -export([convert/2, convert/3]).
@@ -15,7 +15,12 @@
 % @doc Load the nif
 %
 init() ->
-    ok = erlang:load_nif(code:priv_dir(eiconv) ++ "/eiconv_nif", 0).
+    NifName = "eiconv_nif",
+    NifFileName = case code:priv_dir(esqlite) of
+        {error, bad_name} -> filename:join("priv", NifName);
+        Dir -> filename:join(Dir, NifName)
+    end,
+    ok = erlang:load_nif(NifFileName, 0).
 
 % @doc Open a new encoder which can be used to convert text from FromCode into ToCode.
 %
@@ -25,6 +30,16 @@ open(_ToCode, _FromCode) ->
 % @doc Convert Input into the requested encoding.
 %
 conv(_Cd, _Input) ->
+    exit(nif_library_not_loaded).
+
+% @doc Convert a chunk, returns {ok, Converted, Rest}
+%
+chunk(_Cd, _Input) ->
+    exit(nif_library_not_loaded).
+
+% @doc Reset the cd structure, returns{ok, Rest}
+%
+reset(_Cd) ->
     exit(nif_library_not_loaded).
 
 % @doc Close the encoder - dummy function, close will be done by the garbage collector.
